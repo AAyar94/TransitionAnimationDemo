e89665947fae84f307609765768364d4be27bfd4
Mustafa Yiğit <mustafa.yigit@apsiyon.com>
2023-04-10 15:06:17 +0300

    refactor shared element transition

--- app/src/main/java/com/aayar94/transitiondemo/FirstContentFragment.kt
+++ app/src/main/java/com/aayar94/transitiondemo/FirstContentFragment.kt
@@ -1,15 +1,14 @@
 package com.aayar94.transitiondemo
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.doOnPreDraw
-import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.aayar94.transitiondemo.databinding.FragmentFirstContentBinding
-import com.bumptech.glide.Glide
+import com.google.android.material.transition.MaterialElevationScale
 
 
 class FirstContentFragment : Fragment() {
@@ -17,6 +16,12 @@ class FirstContentFragment : Fragment() {
     private val binding get() = _binding!!
     private val adapter: PhotosAdapter by lazy {
         PhotosAdapter { link, extra ->
+            exitTransition = MaterialElevationScale(false).apply {
+                duration = 500
+            }
+            reenterTransition = MaterialElevationScale(true).apply {
+                duration = 500
+            }
             val action =
                 FirstContentFragmentDirections.actionFirstContentFragmentToSecondContentFragment(
                     link
@@ -30,22 +35,33 @@ class FirstContentFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentFirstContentBinding.inflate(layoutInflater, container, false)
-
-        val string = "https://picsum.photos/200"
-        var dataSet: MutableList<String> = mutableListOf()
-        for (i in 0 until 20) {
-            dataSet.add(string)
-        }
-        adapter.setData(dataSet)
-        binding.recyclerView.adapter = adapter
-        postponeEnterTransition()
-        binding.recyclerView.doOnPreDraw {
-            startPostponedEnterTransition()
-        }
-        adapter.notifyDataSetChanged()
-
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        postponeEnterTransition()
+        view.doOnPreDraw { startPostponedEnterTransition() }
+
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        val string1 =
+            "https://plus.unsplash.com/premium_photo-1673254850680-969be808b314?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        val string2 =
+            "https://images.unsplash.com/photo-1680443285773-ef42672d00da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        val string3 =
+            "https://images.unsplash.com/photo-1680770600914-c69d380e2405?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
+
+        val items = List(20) {
+            when (it % 3) {
+                0 -> string1
+                1 -> string2
+                else -> string3
+            }
+        }
+        binding.recyclerView.adapter = adapter.also { it.setData(items) }
+    }
 }
--- app/src/main/java/com/aayar94/transitiondemo/SecondContentFragment.kt
+++ app/src/main/java/com/aayar94/transitiondemo/SecondContentFragment.kt
@@ -1,81 +1,56 @@
 package com.aayar94.transitiondemo
 
-import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.transition.ChangeImageTransform
+import androidx.transition.ChangeTransform
 import androidx.transition.TransitionInflater
-import com.aayar94.transitiondemo.databinding.FragmentFirstContentBinding
 import com.aayar94.transitiondemo.databinding.FragmentSecondContentBinding
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
+import com.google.android.material.transition.MaterialContainerTransform
+import com.google.android.material.transition.MaterialElevationScale
 
 
 class SecondContentFragment : Fragment() {
     private var _binding: FragmentSecondContentBinding? = null
     private val binding get() = _binding!!
-    val args: SecondContentFragmentArgs by navArgs()
+    private val args: SecondContentFragmentArgs by navArgs()
 
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        sharedElementEnterTransition =
-            TransitionInflater.from(requireContext()).inflateTransition(android.R.transition.move)
-        postponeEnterTransition()
-    }
 
+        sharedElementEnterTransition = MaterialContainerTransform().apply {
+            drawingViewId = R.id.fragmentContainerView
+            duration = 500
+            scrimColor = Color.TRANSPARENT
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentSecondContentBinding.inflate(layoutInflater, container, false)
-        postponeEnterTransition()
+        ViewCompat.setTransitionName(binding.imageSecond, args.link)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.imageSecond.apply {
-            transitionName = "imageT"
-                .apply {
-                    Glide.with(binding.imageSecond.context)
-                        .load(args.link)
-                        .listener(object : RequestListener<Drawable>{
-                            override fun onLoadFailed(
-                                e: GlideException?,
-                                model: Any?,
-                                target: Target<Drawable>?,
-                                isFirstResource: Boolean
-                            ): Boolean {
-                                startPostponedEnterTransition()
-                                return false
-                            }
 
-                            override fun onResourceReady(
-                                resource: Drawable?,
-                                model: Any?,
-                                target: Target<Drawable>?,
-                                dataSource: DataSource?,
-                                isFirstResource: Boolean
-                            ): Boolean {
-                                startPostponedEnterTransition()
-                                return false
-                            }
-
-                        })
-                        .into(binding.imageSecond)
-                    startPostponedEnterTransition()
-                }
-        }
+        val imageLink = args.link
+        Glide.with(requireContext())
+            .load(imageLink)
+            .into(binding.imageSecond)
 
         binding.buttonB.setOnClickListener {
             findNavController().navigateUp()
